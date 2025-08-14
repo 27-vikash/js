b55e801d0fc0c79c9ae16be37fcd6a2d86dec7c1
27-vikash <iamvikashkchoudhary@gmail.com>
2025-08-14 17:09:38 +0530

    Delete 01_basics directory

--- 01_basics/demo1.js
+++
@@ -1,1 +0,0 @@
-console.log("hey there,I am vikash and learning javascript.");
--- 01_basics/demo2.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello, World!");
-console.log("hey there, I am Vikash and learning JavaScript.");
-console.log("This is a simple demonstration of JavaScript basics.");
-var greeting = "Welcome to JavaScript!";
-console.log(greeting);
